e415b67e92cb2147ecb49c7d03d236b1e2e03c99
Dave Beck <dacb@uw.edu>
2016-03-08 13:40:29 -0800

    fixed up path to reflect reality

--- globals.sh
+++ globals.sh
@@ -8,7 +8,7 @@ HOST=`mysql_host`
 BWA=/work/software/bwa/bin/bwa
 SAMTOOLS=/work/software/samtools/bin/samtools
 MYSQL=mysql
-MYSQL_HOST=/home/dacb/bin/mysql_host
+MYSQL_HOST=/work/software/bin/mysql_host
 HTSEQ_COUNT=/work/software/htseq/bin/htseq-count
 
 # job parameters
