c0c3b7b587e083e95cd35896bcf92cf2c5066c14
Janet Matsen <janetmatsen@gmail.com>
2016-03-17 14:03:31 -0700

    generalized w\ globals so variants can be tested

--- unused_reads/envoy/troubleshoot.py
+++ unused_reads/envoy/troubleshoot.py
@@ -14,6 +14,21 @@ print("""
     multiline
     """)
 
+SAMPLE = '112'
+RUN_NAME = 'no_sam'
+RUN_BLAST=False
+
+FILE_DIR = './dev-'+RUN_NAME + '/'
+print("directory to save files in: {}".format(FILE_DIR))
+ORIGINAL_BAM = ur.sample_name_to_bam_filepath(SAMPLE)
+DOWNSAMPLED_BAM = FILE_DIR + SAMPLE + '.bam'
+DOWNSAMPLED_FASTA = FILE_DIR + SAMPLE + '.fasta'
+DOWNSAMPLED_TSV = FILE_DIR + SAMPLE + '.tsv'
+
+# make the directory
+ur.create_dir(FILE_DIR)
+
+
 def subsample_bam_to_bam(in_bam, out_bam, downsample_frac):
     # subsample a bam file and save it at the place out_bam.
     # used for developing a faster test of my methods.
@@ -28,12 +43,15 @@ def subsample_bam_to_bam(in_bam, out_bam, downsample_frac):
 
 
 def make_test_bam(downsample_frac=0.02):
-    # grab a .bam file, use samtools to get just 10% of it.
-    out_bam = './dev/downsampled_112.bam'
+    # grab a .bam file, use samtools to get a downsampled fraction.
+    in_bam = ORIGINAL_BAM
+    # prepare a file name to save the result as
+    out_bam = DOWNSAMPLED_BAM
     if not ur.check_file_exists(out_bam):
         subsample_bam_to_bam(
-            in_bam="/gscratch/lidstrom/meta4_bins/workspace/LakWasM112_LOW13_2"
-                   "/bwa/LakWasM112_LOW13_2.sorted.bam",
+            # can wrap line across rows.
+            # Don't use triple quotes or the new lines will be stored.
+            in_bam=in_bam,
             out_bam=out_bam,
             downsample_frac=downsample_frac)
     # todo: report the fraction of the file size.
@@ -48,15 +66,15 @@ def make_test_bam(downsample_frac=0.02):
     # for  /gscratch/lidstrom/meta4_bins/workspace/LakWasM112_LOW13_2/bwa/LakWasM112_LOW13_2.sorted.bam
 
 print("starting directory:")
-ur.shell("ls -lshR ./dev/")
+ur.shell("ls -lshR " + FILE_DIR)
 
 make_test_bam()
 
 
 def test_bam_to_fasta():
     # remove the old fasta if it exists:
-    in_bam = './dev/downsampled_112.bam'
-    out_fasta = './dev/downsampled_112.fasta'
+    in_bam = DOWNSAMPLED_BAM
+    out_fasta = DOWNSAMPLED_FASTA
     if ur.check_file_exists(out_fasta):
         print("file {} exists.  Delete and re-make it.".format(out_fasta))
         os.remove(out_fasta)
@@ -65,9 +83,21 @@ def test_bam_to_fasta():
     ur.bam_to_fasta(source_bam=in_bam, dest_fasta=out_fasta,
                     debug=True, intermediate_file=True)
     # Print the directory contents
-    print('ls -lshR ./dev/')
-    ur.shell("ls -lshR ./dev/")
+    print('ls -lshR {}'.format(FILE_DIR))
+    ur.shell('ls -lshR {}'.format(FILE_DIR))
 
 
 test_bam_to_fasta()
+
+
+def test_blast_fasta():
+    # blast the .fasta file made by test_bam_to_fasta()
+    in_fasta = DOWNSAMPLED_FASTA
+    out_tsv = DOWNSAMPLED_FASTA
+    ur.blast_fasta(in_fasta, out_tsv, outfmt=None)
+    pass
+
+# BLAST takes a long time to run.  Only run it if it was specified at the top.
+if RUN_BLAST:
+    test_blast_fasta()
 
