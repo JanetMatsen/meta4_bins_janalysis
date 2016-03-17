8ad1976aca129dcae5bb4e9dcd82efd9386c97b5
Janet Matsen <janetmatsen@gmail.com>
2016-03-16 21:51:43 -0700

    made smaller .bam file but not as small as expected

--- unused_reads/envoy/troubleshoot.py
+++ unused_reads/envoy/troubleshoot.py
@@ -19,7 +19,7 @@ def subsample_bam_to_bam(in_bam, out_bam):
         pass
     #command = '/work/software/samtools/bin/samtools view -f 256 -h  '
     command = """/work/software/samtools/bin/samtools \
-                 view -s 0.01 {}""".format(in_bam)
+                 view -s 0.001 {}""".format(in_bam)
     print("command to run: {}".format(command))
     # - > ./fasta_files/57_HOW8_2--subsampled.fasta"
     ur.shell(command, outfile=out_bam, debug=False)
@@ -32,6 +32,14 @@ def make_test_bam():
                "/bwa/LakWasM112_LOW13_2.sorted.bam",
         out_bam='./dev/tenth_112.bam')
     pass
+# ls -l /gscratch/lidstrom/meta4_bins/workspace/LakWasM112_LOW13_2/bwa/LakWasM112_LOW13_2.sorted.bam
+
+# (py2)jmatsen@waffle:/gscratch/lidstrom/meta4_bins/janalysis/unused_reads
+## /envoy$ ls -l ./dev/tenth_112.bam #
+#-rw-r--r-- 1 jmatsen users 204,244,448 Mar 16 21:29 ./dev/tenth_112.bam #
+#(py2)jmatsen@waffle:/gscratch/lidstrom/meta4_bins/janalysis/unused_reads # /envoy$ ls -l /gscratch/lidstrom/meta4_bins/workspace/LakWasM112_LOW13_2/bwa/LakWasM112_LOW13_2.sorted.bam
+# -rwxrwx--- 1 dacb users 3,162,655,566 Feb  5 19:49
+# /gscratch/lidstrom/meta4_bins/workspace/LakWasM112_LOW13_2/bwa/LakWasM112_LOW13_2.sorted.bam
 
 make_test_bam()
 
