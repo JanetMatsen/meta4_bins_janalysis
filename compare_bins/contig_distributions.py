ffe1493187667f68cfe31c92877a910b770c8b05
Janet Matsen <janetmatsen@gmail.com>
2016-04-06 16:50:05 -0700

    names and number of contigs in each genome bin
    uses genome_bins.fasta rows that start with > and
    parses with re, and is handled py Pandas.

---
+++ compare_bins/contig_distributions.py
@@ -0,0 +1,52 @@
+import re
+
+import pandas as pd
+
+
+def extract_contig_ID(string):
+    # 1:>Ga0081607_1001 [organism=Methylobac... --> Ga0081607_1001
+    return re.search("Ga[0-9]+_[0-9]+", string).group(0)
+
+
+def extract_contig_number(string):
+    # Ga0081607_1001 --> 1001
+    # todo: why do they start with 1000 ?
+    return re.search("Ga[0-9]+_([0-9]+)", string).group(1)
+
+
+def extract_bin_number(string):
+    # Ga0081607_1001 --> Ga0081607
+    return re.search("(Ga[0-9]+)_[0-9]+", string).group(1)
+
+
+def extract_common_name(string):
+    # [organism=Methylophilus methylotrophus-127-1 (UID203)]
+    # --> Methylophilus methylotrophus-127-1
+    # need to escape the brackets or I get a unbalanced parentheses warning.
+    return re.search("\[organism=([\w\s-]+) \(UID[0-9]+\)\]", string).group(1)
+
+
+def prepare_summary_df(filepath = "./DNA_names.txt"):
+    df = pd.read_csv(filepath, sep='\t', names=['row name'])
+    # Extract the contig name (e.g. Ga0081607_1001)
+    df['contig ID'] = df['row name'].apply(extract_contig_ID)
+    df['contig number'] = df['contig ID'].apply(extract_contig_number)
+    df['bin'] = df['contig ID'].apply(extract_bin_number)
+    df['name'] = df['row name'].apply(extract_common_name)
+    return df
+
+
+def summarise_df(df):
+    summary = df[['bin', 'name']].drop_duplicates().reset_index()[['bin', 'name']]
+    # get the contig counts
+    num_contigs_in_bins = pd.DataFrame(
+        df[['bin', 'contig number']].groupby(
+            'bin')['contig number'].count().reset_index())
+    num_contigs_in_bins.rename(columns={'contig number':'contig count'}, inplace=True)
+    summary = pd.merge(summary, num_contigs_in_bins)
+    return summary
+
+
+
+
+
---
+++ compare_bins/extract_names.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ag --max-count 9999999 ">" /gscratch/lidstrom/meta4_bins/data/genome_bins.fasta > /gscratch/lidstrom/meta4_bins/janalysis/compare_bins/DNA_names.txt
+
