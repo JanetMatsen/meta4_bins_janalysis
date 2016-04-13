51229d6b25c2cbc311ba39b3955857442d58477a
Janet Matsen <janetmatsen@gmail.com>
2016-04-13 16:47:56 -0700

    making .coords for all bins

--- compare_bins/mummer_all_bins.py
+++ compare_bins/mummer_all_bins.py
@@ -13,9 +13,6 @@ print(bins.head())
 # result could be enough for both.
 # Do all to all, even though some comparisons are not very meaningful.
 
-# todo: could do some subset of the groups.
-groups = []
-
 bins_list = bins['file name'].tolist()
 # print('bins_list: {}'.format(bins_list))
 # print('type fo bins_list: {}'.format(type(bins_list)))
@@ -25,47 +22,48 @@ if not os.path.exists(results_dir):
     os.mkdir(results_dir)
 
 def bin_names_to_coords_filepath(bin1, bin2):
+    """
+    prepare a file path for results
+    :param bin1: bin number 1 name/filepath (reference sequence)
+    :param bin2: bin number 2 name/filepath.  (query sequence)
+    :return:string like Acidovora-69x_Ga0081644_to_Acidovorax-79_Ga0081651
+    """
     outpath = results_dir
     return outpath + '/' + bin1 + "_to_" + bin2
 
 
-debug_max_loops = 2
-for bin1_name in bins_list:
-    debug_max_loops -= 1
-    if debug_max_loops <= 0:
-        break
-    print('bin_1 name: {}'.format(bin1_name))
-    bins_to_compare_to = bins_list.copy()
+def mummer_all_bins():
+    """
+    run mummer on all pairs of bins.  Saves results to ./mummer_results/
 
-    debug_max_loops2 = 3
+    :return:
+    """
+    for bin1_name in bins_list:
+        # prepare to iterate over all the bins
+        bins_to_compare_to = bins_list.copy()
 
-    for bin2_name in bins_to_compare_to:
+        # loop over all the bins, including itself.
+        for bin2_name in bins_to_compare_to:
 
-        # set up temp loop limiter
-        debug_max_loops2 -= 1
-        if debug_max_loops2 <= 0:
-            break
+            # prepare a prefix for mummer to use.
+            delta_prefix = bin_names_to_coords_filepath(bin1_name, bin2_name)
+            print('.delta file prefix: {}'.format(delta_prefix))
 
-        print('     ' + bin2_name)
+            # Make the .delta file
+            # for now allow mummer to run against itself; this is a control.
+            # run these commands:
+            subprocess.check_call(
+                ['/gscratch/lidstrom/software/MUMmer3.23/nucmer',
+                '--prefix={}'.format(delta_prefix),
+                './individual_bins/' + bin1_name + '.fasta',
+                './individual_bins/' + bin2_name + '.fasta'])
 
-        # prepare a prefix for mummer to use.
-        delta_prefix = bin_names_to_coords_filepath(bin1_name, bin2_name)
-        print('.delta file prefix: {}'.format(delta_prefix))
+            coords_path = open(delta_prefix + '.coords', 'w')
+            print('coords_path: {}'.format(coords_path))
+            subprocess.check_call(
+                ['/gscratch/lidstrom/software/MUMmer3.23/show-coords',
+                 '-rcl', str(delta_prefix + '.delta')],
+                stdout=coords_path)
 
-        # print('bins_to_compare_to: {}'.format(bins_to_compare_to))
-
-        # Make the .delta file
-        # for now allow mummer to run against itself; this is a control.
-        # run these commands:
-        subprocess.check_call(
-            ['/gscratch/lidstrom/software/MUMmer3.23/nucmer',
-            '--prefix={}'.format(delta_prefix),
-            './individual_bins/' + bin1_name + '.fasta',
-            './individual_bins/' + bin2_name + '.fasta'])
-
-        coords_path = open(delta_prefix + '.coords', 'w')
-        print('coords_path: {}'.format(coords_path))
-        subprocess.check_call(
-            ['/gscratch/lidstrom/software/MUMmer3.23/show-coords',
-             '-rcl', str(delta_prefix + '.delta')],
-            stdout=coords_path)
+if __name__ == '__main__':
+    mummer_all_bins()
