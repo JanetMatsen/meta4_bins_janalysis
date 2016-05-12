118fc45172275243599da053bf080880d1105922
Janet Matsen <janetmatsen@gmail.com>
2016-05-11 19:14:41 -0700

    script to get Dave's bins
    also had a negligible change in get_isolate,
    but renamed dir for isolates

---
+++ compare_bins/bins/get_dave_bins/get_dave_bins.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+# bins are stored at :
+# e.g. head /work/dacb/elvizAnalysis/checkM_individual/results/elviz-contigs-1056274.Methylomonas-1/elviz-contigs-1056274.Methylomonas-1.fna
+
+# the favorite bins (540 of them) were e-mailed to Janet on 5/10
+# Janet saved them locally, moved a copy in .tsv format via sshfs, and is grabbing them from /work/dacb/elvizAnalysis/.
+
+BIN_NAMES=`awk 'NR>1 {print $1}' 160510_interesting_bins_dave_made.tsv` 
+#echo $BIN_NAMES
+PATH_TO_BINS="/work/dacb/elvizAnalysis/results/"
+
+# keep track of bins we couldn't find (shouldn't be any)
+MISSING_BINS=""
+
+# Grab them one by one
+for BIN in $BIN_NAMES
+do 
+  BIN_PATH="${PATH_TO_BINS}${BIN}.fna"
+  #echo $BIN_PATH""
+  #ls -l $BIN_PATH
+  cp $BIN_PATH ../fasta_files
+  # if the exit status wasn't zero, store 
+  if [ $? -ne 0 ]
+  then
+      MISSING_BINS=${MISSING_BINS} ${BIN}
+  fi
+done
+
+echo "missing bins:"
+echo $MISSING_BINS
+
--- compare_bins/bins/get_isolate/get_isolate_bins.sh
+++ compare_bins/bins/get_isolate/get_isolate_bins.sh
@@ -1,8 +1,10 @@
+DESTINATION=../fasta_files/
+
 # Loop over the bins that we want and make a copy of each in this dir. 
 while read line; do
   file_name=$line
   file_path="/work/dacb/extract_isolates/${file_name}"
   ls -l $file_path
 	#"/work/dacb/extract_isolates/${file_name}"
-  cp $file_path . 
+  cp $file_path $DESTINATION
 done < isolate_bins_to_get
