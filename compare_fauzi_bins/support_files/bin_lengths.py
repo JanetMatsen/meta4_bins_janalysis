7c494e97a2954f0bf82a231c11f471254b09a94e
Janet Matsen <janetmatsen@gmail.com>
2016-06-01 07:27:40 -0700

    include the bin name for merging with bin_stats.tsv

--- compare_fauzi_bins/support_files/bin_lengths.py
+++ compare_fauzi_bins/support_files/bin_lengths.py
@@ -25,6 +25,13 @@ def bin_base_pairs(filepath):
     return {'contigs':contigs, 'bp':bp}
 
 
+def extract_bin_number(string):
+    # Ga0081607_1001 --> Ga0081607
+    m = re.search("(Ga[0-9]+)", string)
+    assert m, 'no match found in {}'.format(string)
+    return m.group(1)
+
+
 def walk_bins():
     results = pd.DataFrame()
     global BIN_PATH
@@ -33,11 +40,14 @@ def walk_bins():
         filename = re.search('([A-Za-z0-9_-]+.fasta)', bin_path).group(0)
         print(bin_path)
         print(filename)
+        # Get a dict of info about contigs, bp
         stats = bin_base_pairs(bin_path)
         print(stats)
         df_row = pd.DataFrame({k:[v] for k, v in stats.items()})
         # add on bin filename:
         df_row['file name'] = filename
+        # add on a bin number.
+        df_row['bin'] = extract_bin_number(filename)
         if results.empty:
             print('start new dataframe')
             results = df_row
